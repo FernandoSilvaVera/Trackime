30de20a8df1d9d4d18405281d875127b23bd646d
fsvera <fernandosilv4c@gmail.com>
2018-01-24 20:03:05 +0100

    Controlador del login

--- php/controlador/logearse.php
+++ php/controlador/logearse.php
@@ -5,17 +5,25 @@ class Logearse{
 	public function __construct(){	
 	}
 
-
 	public function analizar($url){
+
+		$bbdd = new BBDD;
+		$usuarios = $bbdd->obtener("select * from USUARIOS",$bbdd->usuarios);
+		$tiene_acceso = false;
 
 		if(!isset($_SESSION))
 			session_start();
-		
-		if(!isset($_SESSION["login"])){
-			$_SESSION["login"] = "Fernando";
-		}
 
-		return "../vista/login.php";
+		if(!isset($_SESSION["login"]))
+			foreach($usuarios as $buscar)
+				if($buscar->dato["usuario"] === $_POST["user"])
+					if($buscar->dato["contrasena"] == $_POST["pswd"]){
+						$_SESSION["login"] = $buscar->dato["usuario"];
+						$tiene_acceso = true;	
+					}
+
+		return $tiene_acceso ?"../../index.php" : "../vista/login.php";
+
 	}
 
 }
