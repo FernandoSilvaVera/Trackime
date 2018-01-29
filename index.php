66cc1983686ed1898ac28c61f3288281e48cc89b
fsvera <fernandosilv4c@gmail.com>
2018-01-29 18:46:52 +0100

    Update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body>
 	<div class="container">
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="javascript:void(0)">AnimeTracker</a>
+			<a class="navbar-brand" href="./index.php">AnimeTracker</a>
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
 				<span class="navbar-toggler-icon"></span>
 			</button>
