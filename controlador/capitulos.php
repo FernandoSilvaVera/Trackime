f8d6b5a9257852c2a69a68988c0d24224a234c97
fsvera <fernandosilv4c@gmail.com>
2018-01-30 17:58:36 +0100

    Controlador para generar los capitulos y el nombre de la serie

---
+++ controlador/capitulos.php
@@ -0,0 +1,29 @@
+<?PHP
+
+require_once("../modelo/BBDD.php");
+
+class Capitulos{
+
+	private $bbdd;
+
+	public function __construct(){
+		$this->bbdd = new BBDD;
+		$this->analizar();
+	}
+
+	public function analizar(){
+	
+		if(!isset($_SESSION))
+			session_start();
+
+		$id = $_REQUEST["id"];
+		$capitulos= "SELECT nombre,capitulos,id from ANIMES where id=$id";
+		$_SESSION["capitulos"] = $this->bbdd->obtener($capitulos,array("nombre","capitulos","id"));
+		
+	}
+
+}
+
+new Capitulos;
+
+?>
