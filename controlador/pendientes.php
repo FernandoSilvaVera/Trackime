38b8975489464c7877f8062492a40cb0fa88dd54
fsvera <fernandosilv4c@gmail.com>
2018-02-11 20:10:01 +0100

    Ya es funcional

--- controlador/pendientes.php
+++ controlador/pendientes.php
@@ -5,61 +5,46 @@ require_once("../modelo/BBDD.php");
 class Paginacion{
 
 	private $bbdd;
-	private $inicio;
-	private $fin;
-	private $max = 12;
+	private const TAM_PAGINAS = 12;
+	private $paginaActual;
 
 	public function __construct(){
 		$this->bbdd = new BBDD;
-		$this->inicio = $_REQUEST["id"] * $this->max;
-		$this->fin = $this->inicio + $this->max;
 		$this->analizar();
 	}
 
+
+
+
+	private function totalPaginas(){
+		$this->paginaActual = empty($_REQUEST["id"]) ?0 :$_REQUEST["id"];
+		$max = 'select count(*) as max from CUSTOM join ANIMES where CUSTOM.nombre_anime = ANIMES.nombre and CUSTOM.estado = "pendiente"';
+		$numFilas = $this->bbdd->obtener($max,array("max"));
+		return ceil($numFilas[0]->dato["max"] / $this::TAM_PAGINAS);
+	}
+
 	private function obtenerPaginacion(){
-
-		$id_0 = $_REQUEST["id"];
-		$id_1 = $_REQUEST["id"] +1;
-		$id_2 = $_REQUEST["id"] +2;
-
 		$devolver = array();
-			array_push($devolver,'<li class="page-item disabled"><a class="page-link" href="./pendientes.php?id='.($id_0-1).'">Anterior</a></li>');
-			array_push($devolver,'<li class="page-item"><a class="page-link" href="./pendientes.php?id='.($id_0-1).'">'.$id_0.'</a></li>');
-			array_push($devolver,'<li class="page-item active"><a class="page-link" href="./pendientes.php?id='.($id_1-1).'">'.$id_1.'</a></li>');
-			array_push($devolver,'<li class="page-item"><a class="page-link" href="./pendientes.php?id='.($id_2-1).'">'.$id_2.'</a></li>');
-			array_push($devolver,'<li class="page-item"><a class="page-link" href="./pendientes.php?id='.($id_2-1).'">Siguiente</a></li>');
+		for($i=0; $i<$this->totalPaginas(); $i++)
+			if($i == $this->paginaActual)
+				array_push($devolver,'<li class="page-item active"><a class="page-link" href="./pendientes.php?id='.$i.'">'.($i+1).'</a></li>');
+			else
+				array_push($devolver,'<li class="page-item"><a class="page-link" href="./pendientes.php?id='.$i.'">'.($i+1).'</a></li>');
 		return $devolver;
 	}
 
-	private function obtenerPaginacionInicial(){
-		$devolver = array();
-			array_push($devolver,'<li class="page-item disabled"><a class="page-link" href="#">Anterior</a></li>');
-			array_push($devolver,'<li class="page-item active"><a class="page-link" href="./pendientes.php?id=0">1</a></li>');
-			array_push($devolver,'<li class="page-item"><a class="page-link" href="./pendientes.php?id=1">2</a></li>');
-			array_push($devolver,'<li class="page-item"><a class="page-link" href="./pendientes.php?id=2">3</a></li>');
-			array_push($devolver,'<li class="page-item"><a class="page-link" href="./pendientes.php?id=1">Siguiente</a></li>');
-		return $devolver;
-	}
-
-	private function obtenerSeries(){
-
-		$series = "select ANIMES.id,ANIMES.nombre from CUSTOM join ANIMES where CUSTOM.nombre_anime = ANIMES.nombre and CUSTOM.estado='pendiente'";
-		//$series= "SELECT ANIMES.nombre,id from ANIMES where id > $this->inicio and id <= $this->fin";
-		return $this->bbdd->obtener($series,array("id","nombre"));
+	private function obtenerSerie(){
+		$comienzo = $this->paginaActual * $this::TAM_PAGINAS;
+		$rango = "select ANIMES.id,ANIMES.nombre from CUSTOM join ANIMES where CUSTOM.nombre_anime = ANIMES.nombre and CUSTOM.estado='pendiente' limit $comienzo," . $this::TAM_PAGINAS;
+		return $this->bbdd->obtener($rango,array("nombre","id"));
 	}
 
 	public function analizar(){
-
 		if(!isset($_SESSION))
 			session_start();
 
-		$_SESSION["series"] = $this->obtenerSeries(); 
-
-		if(empty($_REQUEST["id"]))
-			$_SESSION["paginacion"] = $this->obtenerPaginacionInicial();
-		else
-			$_SESSION["paginacion"] = $this->obtenerPaginacion();
-
+		$_SESSION["paginacion"] = $this->obtenerPaginacion();
+		$_SESSION["series"] = $this->obtenerSerie();
 	}
 }
 
