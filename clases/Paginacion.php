152d8c100395762944b45ee806a7f38e6d304096
fsvera <fernandosilv4c@gmail.com>
2018-02-17 17:57:53 +0100

    update

--- clases/Paginacion.php
+++ clases/Paginacion.php
@@ -17,8 +17,12 @@ class Paginacion{
 		$this->bbdd = new BBDD;
 	}
 
-	protected function totalPaginas($from){
-		$numFilas = $this->bbdd->obtener("SELECT count(*) as max $from", array("max"));
+	protected function totalPaginas($from, $usuario){
+		if(is_null($usuario))
+			$numFilas = $this->bbdd->obtener("SELECT count(*) as max $from", array("max"));
+		else
+			$numFilas = $this->bbdd->obtener("SELECT count(*) as max $from '$usuario'", array("max"));
+
 		$this->maxPaginas = ceil($numFilas[0]->dato["max"] / $this::TAM_PAGINAS);
 	}
 
@@ -30,11 +34,16 @@ class Paginacion{
 				array_push($this->paginas,'<li class="page-item"><a class="page-link" href="./'.$vista.'?id='.$i.'">'.($i+1).'</a></li>');
 	}
 
-	protected function obtenerSerie($consulta){
+	protected function obtenerSerie($consulta, $usuario){
 		$comienzo = $this->paginaActual * $this::TAM_PAGINAS;
-		$rango = "$consulta[select] $consulta[from] limit $comienzo," . $this::TAM_PAGINAS;
+		if(is_null($usuario))
+			$rango = "$consulta[select] $consulta[from] limit $comienzo," . $this::TAM_PAGINAS;
+		else
+			$rango = "$consulta[select] $consulta[from] '$usuario' limit $comienzo," . $this::TAM_PAGINAS;
+
 		return $this->bbdd->obtener($rango,array("nombre","id"));
 	}
+
 }
 
 ?>
