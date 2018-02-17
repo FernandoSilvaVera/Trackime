4cb93bed9d21c10aae4215c13646287c55476e05
fsvera <fernandosilv4c@gmail.com>
2018-02-17 17:58:11 +0100

    Emision

--- controlador/emision.php
+++ controlador/emision.php
@@ -1,49 +1,28 @@
 <?PHP
 
-require_once("../modelo/BBDD.php");
+require_once("../clases/Paginacion.php");
 
-class Paginacion{
+class Emision extends Paginacion{
 
-	private $bbdd;
-	private const TAM_PAGINAS = 12;
-	private $paginaActual;
+	private const VISTA = "emision.php";
+	private const CONSULTA = array(
+		"select" => "select nombre,id ",
+		"from" => "from ANIMES join FECHA where ANIMES.nombre = FECHA.nombre_anime and anio = 2017"
+	); 
 
 	public function __construct(){
-		$this->bbdd = new BBDD;
+		parent::__construct();
+		$this->totalPaginas($this::CONSULTA["from"],null);
+		$this->obtenerPaginacion($this::VISTA);
 		$this->analizar();
 	}
 
-	private function totalPaginas(){
-		$this->paginaActual = empty($_REQUEST["id"]) ?0 :$_REQUEST["id"];
-		$numFilas = $this->bbdd->obtener("SELECT count(*) as max from ANIMES join FECHA where ANIMES.nombre = FECHA.nombre_anime and anio = 2017",array("max"));
-		return ceil($numFilas[0]->dato["max"] / $this::TAM_PAGINAS);
-	}
-
-	private function obtenerPaginacion(){
-		$devolver = array();
-		for($i=0; $i<$this->totalPaginas(); $i++)
-			if($i == $this->paginaActual)
-				array_push($devolver,'<li class="page-item active"><a class="page-link" href="./emision.php?id='.$i.'">'.($i+1).'</a></li>');
-			else
-				array_push($devolver,'<li class="page-item"><a class="page-link" href="./emision.php?id='.$i.'">'.($i+1).'</a></li>');
-		return $devolver;
-	}
-
-	private function obtenerSerie(){
-		$comienzo = $this->paginaActual * $this::TAM_PAGINAS;
-		$rango = "SELECT nombre,id from ANIMES join FECHA where ANIMES.nombre = FECHA.nombre_anime and anio = 2017 limit $comienzo," . $this::TAM_PAGINAS;
-		return $this->bbdd->obtener($rango,array("nombre","id"));
-	}
-
 	public function analizar(){
-		if(!isset($_SESSION))
-			session_start();
-
-		$_SESSION["paginacion"] = $this->obtenerPaginacion();
-		$_SESSION["series"] = $this->obtenerSerie();
+		$_SESSION["paginacion"] = $this->paginas;
+		$_SESSION["series"] = $this->obtenerSerie($this::CONSULTA, null);
 	}
 }
 
-new Paginacion;
+new Emision;
 
 ?>
