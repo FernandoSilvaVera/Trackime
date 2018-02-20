06251484eb231823bf825d85a1458b471ef96e26
fsvera <fernandosilv4c@gmail.com>
2018-02-20 16:08:43 +0100

    Agregada la caja de comentarios

---
+++ controlador/visor.php
@@ -0,0 +1,26 @@
+<?PHP
+
+require_once("../modelo/BBDD.php");
+
+class Comentarios {
+
+	private $bbdd;
+	private $comentarios;
+
+	public function __construct(){
+		session_start();
+		$this->bbdd = new BBDD;
+		$this->Comentarios = array();
+	}
+
+	public function obtenerComentarios(){
+		return $this->bbdd->obtener("select usuario,comentario from COMENTARIOS where nombre_anime='$_REQUEST[anime]' and capitulo=$_REQUEST[cap]",array("usuario","comentario"));
+	}
+
+}
+
+
+$comentarios = new Comentarios;
+$comentario = $comentarios->obtenerComentarios();
+
+?>
