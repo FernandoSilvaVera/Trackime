9b9c1b5850cc51b66222ef2a01e5707423bb5419
fsvera <fernandosilv4c@gmail.com>
2018-02-20 16:06:15 +0100

    Pequeña mejora en el codigo

--- controlador/agregar.php
+++ controlador/agregar.php
@@ -7,21 +7,17 @@ class Agregar{
 	private $bbdd;
 
 	public function __construct(){
+		session_start();
 		$this->bbdd = new BBDD;
 		$this->analizar();
 	}
 
 	public function analizar(){
-	
-		if(!isset($_SESSION))
-			session_start();
-
 		$usuario = $_SESSION["login"];
 		$serie = $_GET["serie"];
 		$estado = $_GET["estado"];
 		$datos = 'insert into CUSTOM (usuario,nombre_anime,estado) values("'.$usuario.'","'.$serie.'","'.$estado.'")';
 		$this->bbdd->meter($datos);
-
 	}
 
 }
