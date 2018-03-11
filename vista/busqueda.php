6762fddf410a1a431ceb4d2c9013e5e3525f217c
fsvera <fernandosilv4c@gmail.com>
2018-03-11 18:10:23 +0100

    Mejorada los enlaces

--- vista/busqueda.php
+++ vista/busqueda.php
@@ -12,23 +12,25 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+	
+	<script src="../javascript/ajax.js"> </script>
+	
+
 </head>
 <body>
 
     <!-- Barra de navegacion -->
 	<?php require "./utils/navegacion.php"?>
 
-	<br>
-
 	<div class="container">
 		<?php foreach ($datos as $serie): ?>
 		<div class="row justify-content-center align-items-center mt-4">
 			<div class="col-sm-3">
 				<div class="card">
-					<a href="#"><img class="card-img-top rounded" src="../images/<?=$serie->dato["id"] ?>.jpg"></a>
+					<a href="./capitulos.php?id=<?=$serie->dato["id"]?>"><img class="card-img-top rounded" src="../images/<?=$serie->dato["id"] ?>.jpg"></a>
 				</div>
 			</div>
-			<div class="col-sm-6">
+			<div id="<?=$serie->dato["id"]?>" style="cursor:pointer" class="col-sm-7 descripcion">
 				<h1><?= $serie->dato["nombre"]?></h1>
 				<p>Descripcion del anime</p>
 				<span class="badge badge-dark">Mecha</span>
@@ -36,7 +38,7 @@
 				<span class="badge badge-dark">Recuerdon de la vida</span>
 				<span class="badge badge-dark">Comedia</span>
 			</div>
-			<div class="col-sm-3 text-center">
+			<div class="col-sm-2 text-center">
 				<button type="button" class="btn btn-dark">Seguir</button>
 			</div>
 		</div> 
