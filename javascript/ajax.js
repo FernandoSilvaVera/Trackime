22978d9b2fd35d96a0d1ab350badf3c3f62afd6d
fsvera <fernandosilv4c@gmail.com>
2018-03-11 18:14:28 +0100

    ajax

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -8,27 +8,33 @@ $.request = function(parametro){
 //Marca la serie como pendiente
 $(document).ready(function(){
     $("#pendiente").click(function(){
-	$.get("../controlador/agregar.php",{serie:$.request('anime'),estado:"pendiente"}, function(respuesta){
-		//TODO -->Cambiar icono de agregar
-	})
+		$.get("../controlador/agregar.php",{serie:$.request('anime'),estado:"pendiente"}, function(respuesta){
+			//TODO -->Cambiar icono de agregar
+		})
     })
 })
 
 //Marca la serie como terminada
 $(document).ready(function(){
     $("#terminada").click(function(){
-	$.get("../controlador/agregar.php",{serie:$.request('anime'),estado:"terminada"}, function(respuesta){
-		//TODO -->Cambiar icono de agregar
-	})
+		$.get("../controlador/agregar.php",{serie:$.request('anime'),estado:"terminada"}, function(respuesta){
+			//TODO -->Cambiar icono de agregar
+		})
     })
 })
 
 //Escribe el comentario en la bbdd
 $(document).ready(function(){
     $("#comentar").click(function(){
-	$.get("../controlador/comentar.php",{serie:$.request('anime'),capitulo:$.request('cap'),comentario:$('#comentario').val()}, function(respuesta){
-		//TODO
-	})
+		$.get("../controlador/comentar.php",{serie:$.request('anime'),capitulo:$.request('cap'),comentario:$('#comentario').val()}, function(respuesta){
+			//TODO
+		})
     })
 })
 
+//Redirige a la vista de capitulos desde la página de busqueda
+$(document).ready(function(){
+    $(".descripcion").click(function(){
+		window.location.href = '/Trackime/vista/capitulos.php?id=' + this.id;
+    })
+})
