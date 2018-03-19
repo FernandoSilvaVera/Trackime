df891189ac06619d87cf9fb76afa8835919f31dd
ShuviDola <fernandosilv4c@gmail.com>
2018-03-19 20:03:11 +0100

    Ahora busca usuarios

--- controlador/busqueda.php
+++ controlador/busqueda.php
@@ -14,19 +14,26 @@ class Busqueda{
 	}
 
 	public function busquedaAnimes(){
-		return $this->bbdd->obtener("select id,nombre from ANIMES where nombre like '%$this->busqueda%'",["id","nombre"]);
+		return $this->bbdd->obtener("select id,nombre from ANIMES where nombre like '%$this->busqueda%'", ["id","nombre"]);
 	}
 
 	public function busquedaGenero($serie){
-		return $this->bbdd->obtener("select nombre,genero from GENERO where nombre='$serie'",["nombre","genero"]);
+		return $this->bbdd->obtener("select nombre,genero from GENERO where nombre='$serie'", ["nombre","genero"]);
+	}
+
+	public function busquedaUsuarios(){
+		return $this->bbdd->obtener("select usuario,imagen from USUARIOS where usuario like '%$this->busqueda%'", ["usuario","imagen"]);
 	}
 
 }
 
 $busqueda = new Busqueda($_REQUEST["busqueda"]);
-$datos = $busqueda->busquedaAnimes();
+$animes = $busqueda->busquedaAnimes();
 
-foreach($datos as $anime)
+foreach($animes as $anime)
 	$genero[$anime->dato["nombre"]] = $busqueda->busquedaGenero($anime->dato["nombre"]);
+
+$usuarios = $busqueda->busquedaUsuarios();
+
 
 ?>
--- vista/busqueda.php
+++ vista/busqueda.php
@@ -12,36 +12,82 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-	
 	<script src="../javascript/ajax.js"> </script>
-	
-
 </head>
 <body>
 
     <!-- Barra de navegacion -->
 	<?php require "./utils/navegacion.php"?>
 
+	<!-- Genera los usuarios-->
+
 	<div class="container">
-		<?php foreach($datos as $anime):?>
-		<div class="row justify-content-center align-items-center mt-4">
-			<div class="col-sm-3">
-				<div class="card">
+
+		<?php foreach($usuarios as $usuario):?>
+
+			<div class="row justify-content-center align-items-center mt-4">
+
+				<!-- Imagen del usuario-->
+
+				<div class="col-sm-3" align="center">
+					<div class="col-sm-9">
+						<a href="./perfil?usuario=<?=$usuario->dato["usuario"]?>"><img class="card-img-top rounded" src="../images/usuario/<?=$usuario->dato["imagen"] ?>.png"></a>
+					</div>
+				</div>
+
+				<!-- Descripcion del usuario-->
+
+				<div id="<?=$usuario->dato["usuario"]?>" style="cursor:pointer" class="col-sm-7 perfil">
+					<h1><?= $usuario->dato["usuario"]?></h1>
+					<p>Descripcion del usuario</p>
+				</div>
+	
+				<!-- Boton para seguir al usuario-->
+
+				<div class="col-sm-2 text-center">
+					<button type="button" class="btn btn-dark">Seguir</button>
+				</div>
+
+			</div> 
+
+		<?php endforeach;?>
+
+	</div>
+
+	<!-- Genera los animes-->
+
+	<div class="container">
+
+		<?php foreach($animes as $anime):?>
+
+			<div class="row justify-content-center align-items-center mt-4">
+
+				<!-- Imagen del anime-->
+
+				<div class="col-sm-3" align="center">
 					<a href="./capitulos.php?id=<?=$anime->dato["id"]?>"><img class="card-img-top rounded" src="../images/<?=$anime->dato["id"] ?>.jpg"></a>
 				</div>
-			</div>
-			<div id="<?=$anime->dato["id"]?>" style="cursor:pointer" class="col-sm-7 descripcion">
-				<h1><?= $anime->dato["nombre"]?></h1>
-				<p>Descripcion del anime</p>
-				<?php foreach($genero[$anime->dato["nombre"]] as $aux):?>
-				<span class="badge badge-dark"><?=$aux->dato["genero"]?></span>
-				<?php endforeach;?>
-			</div>
-			<div class="col-sm-2 text-center">
-				<button type="button" class="btn btn-dark">Seguir</button>
-			</div>
-		</div> 
+
+				<!-- Descripcion del anime-->
+
+				<div id="<?=$anime->dato["id"]?>" style="cursor:pointer" class="col-sm-7 descripcion">
+					<h1><?= $anime->dato["nombre"]?></h1>
+					<p>Descripcion del anime</p>
+					<?php foreach($genero[$anime->dato["nombre"]] as $aux):?>
+					<span class="badge badge-dark"><?=$aux->dato["genero"]?></span>
+					<?php endforeach;?>
+				</div>
+
+				<!-- Boton para agregar el anime-->
+
+				<div class="col-sm-2 text-center">
+					<button type="button" class="btn btn-dark">Agregar</button>
+				</div>
+
+			</div> 
+
 		<?php endforeach;?>
+
 	</div>
 
 </body>
