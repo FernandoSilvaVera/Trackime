434d397d98cb213c310fb72e4cba31a3b3ae5865
ShuviDola <fernandosilv4c@gmail.com>
2018-03-26 12:58:17 +0200

    conexion con genero

--- app/Custom.php
+++ app/Custom.php
@@ -8,4 +8,10 @@ class custom extends Model
 {
     public $timestamps = false;
 	protected $table = "custom";
+
+	public function genre()
+	{
+		return $this->belongsToMany('Trackime\GenreAnime','animes','anime','anime','anime','anime');
+	}
+
 }
