a733bead9f176d0d1b8cfbbf08c843b0cdf3ac01
ShuviDola <fernandosilv4c@gmail.com>
2018-03-26 12:59:59 +0200

    added the gender

--- resources/views/animes/list.blade.php
+++ resources/views/animes/list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-7">
+        <div class="col-6">
 			<h3 class="text-center">Listado de capitulos</h3>
 			<table class="table table-hover">
 				<tbody>
@@ -16,15 +16,23 @@
 				</tbody>
 			</table>
 		</div>
-        <div class="col-md-5 text-center">
+
+		<!-- Parte derecha de la pagina-->
+
+        <div class="col-6 text-center">
+			<img class="card-img-top col-md-10 mb-1" src="{{ asset('images/5.jpg') }}"/>
 			<h3 id="name_anime">{{ $anime['anime'] }}</h3>
-			<p class="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			@foreach($anime->genre as $genre)
+				<span class="badge badge-dark">{{ $genre['genre']}}</span>
+			@endforeach
+			<p class="text-justify mt-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 
 			<!-- Marcar como pendiente/terminada-->
 			@guest
 			@else
 			 <div class="text-right">
 				<button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown">Agregar</button>
+
 				<div class="dropdown-menu">
 					<a id="pendiente" class="dropdown-item">como pendiente</a>
 					<a id="terminada" class="dropdown-item">como terminada</a>
--- resources/views/layouts/series.blade.php
+++ resources/views/layouts/series.blade.php
@@ -4,16 +4,24 @@
 <div class="container text-center">
     <div class="row">
 		@foreach($animes as $anime)
-        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-6 mb-5">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6 mb-5">
 			<div class="card">
 				<a href="{{ url('/animes/') . '/' . $anime['anime'] }}">
 					<img class="card-img-top" src="{{ asset('images/5.jpg') }}" >
 				</a>
-				<div class="card-footer">{{$anime['anime']}}</div>
+				<div class="card-footer" style="background-color:#ffffff">
+					{{$anime['anime']}}<br>
+					<div class="text-left">
+					@foreach($anime->genre as $genre)
+						<span class="badge badge-dark">{{ $genre['genre'] }}</span>
+					@endforeach
+					</div>
+				</div>
+			
 			</div>
         </div>
 		@endforeach
     </div>
-	{{$animes->links() }}
+	{{ $animes->links()}}
 </div>
 @endsection
