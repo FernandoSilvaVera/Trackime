5b7370517302a7655a1b216e0d3436316f5b06c2
ShuviDola <fernandosilv4c@gmail.com>
2018-03-27 15:51:51 +0200

    now you can search the animes

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,23 @@
 |
 */
 
-Route::get('/', function(){
-	return view("home");
-});
-
 Route::get('/animes', 'AnimeController@index'); 
 Route::get('/pendientes', 'CustomController@pendientes');
 Route::get('/terminadas', 'CustomController@terminadas');
 Route::get('/animes/{animeName}', 'ListController@index');
 Route::get('/emision', 'EmissionController@index');
+Route::get('/busqueda', 'SearchController@index');
+
+Route::get('/', function(){
+	return view("home");
+});
 
 Route::get('/personaje', function(){
 	return view('animes.character');
 });
+
 Route::get('/aleatorio', function(){
 	return view('animes.random');
-});
-
-Route::get('/busqueda', function(){
-	return view('search');
 });
 
 Route::post('/agregarSerie', 'CustomController@store'); 
@@ -39,4 +37,5 @@ Route::get('/user/{userName}',function($userName){
 })->where('userName', '[A-Za-z]+');
 
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
