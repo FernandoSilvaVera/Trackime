5d19124456b653db8507ee04964eca8ac86fa9a8
ShuviDola <fernandosilv4c@gmail.com>
2018-04-07 18:22:13 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@
 |
 */
 
-//return Redirect::to('https://download2194.mediafire.com/9n6njvj571cg/o4jysxti2f2f7mi/2890_12.mp4');
 Route::get('/video/{anime}/{chapter}', 'VideoController@index');
 Route::get('/animes', 'AnimeController@index'); 
 Route::get('/pendientes', 'CustomController@pendientes');
@@ -24,27 +23,22 @@ Route::get('/busqueda', 'SearchController@index');
 Route::get('/filtro/anime', 'FilterController@anime');
 Route::get('/filtro/personaje', 'FilterController@character');
 Route::get('/administrar', 'AdminController@index');
-Route::get('/test', 'AdminController@updateVideoAmazon');
-
-Route::post('updateChapters', 'AdminController@updateChapters');
-Route::post('saveAnime', 'AdminController@storeAnime');
-Route::post('setPendingVideo', 'AdminController@setPendingVideo');
-Route::post('updateEmission', 'AdminController@updateEmission');
-Route::post('saveVideo', 'AdminController@storeVideo');
-
 Route::get('/', function(){
 	return view("home");
 });
-
 Route::get('/aleatorio', function(){
 	return view('random');
 });
-
-Route::post('/agregarSerie', 'CustomController@store'); 
-
 Route::get('/user/{userName}',function($userName){
 	return view("user.user",['userName' => $userName]);
 })->where('userName', '[A-Za-z]+');
+
+
+Route::post('/updateAnimeFLV', 'AdminController@updateVideoAnimeFLV');
+Route::post('saveAnime', 'AdminController@storeAnime');
+Route::post('updateEmission', 'AdminController@updateEmission');
+Route::post('/agregarSerie', 'CustomController@store'); 
+
 
 Auth::routes();
 
