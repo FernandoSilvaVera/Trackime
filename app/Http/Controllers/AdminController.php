0fa0ddbc66fee3d992fee5caf2725e5767f2275e
ShuviDola <fernandosilv4c@gmail.com>
2018-04-12 19:44:08 +0200

    Auth

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,6 +16,10 @@ class AdminController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+		$this->middleware('auth');
+    }
 
     public function index()
 	{
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,10 +13,6 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
