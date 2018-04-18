c04b12128c1a5e5c940a8f79e34616d85a51bfc7
ShuviDola <fernandosilv4c@gmail.com>
2018-04-18 23:21:45 +0200

    mobile

--- resources/views/animes/filter.blade.php
+++ resources/views/animes/filter.blade.php
@@ -4,15 +4,13 @@
 <div class="container">
     <div class="row text-center mt-3 justify-content-center">
 		@foreach($animes as $anime)
-        <div class="col-lg-4 col-md-6 col-10 mb-5">
-			<div class="card">
+        <div class="col-lg-4 col-sm-6 col-10 mb-5">
+			<div class="card mb-1">
 				<a href="{{ url('/animes/') . '/' . $anime->anime }}">
 					<img class="card-img-top" src="{{ asset('images/' . $anime->web) }}.jpg">
 				</a>
-				<div class="card-footer" style="background-color:#ffffff">
-					<h4>{{$anime->anime}}</h4>
-				</div>
 			</div>
+			<span><b>{{ $anime->anime }}</b></span>
         </div>
 		@endforeach
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container text-right">
     <div class="row text-center mt-3 justify-content-center">
 		@foreach($animes as $anime)
-        <div class="col-lg-4 col-md-6 col-10 mb-5">
+        <div class="col-lg-4 col-6 mb-5">
 			<div class="card">
 				<a href="{{ url('/animes/') . '/' . $anime->anime }}">
 					<img class="card-img-top" src="{{ asset('images/' . $anime->image()  ) }}.jpg">
