6f28066f618518adfea16e9c66839c8297416d8e
ShuviDola <fernandosilv4c@gmail.com>
2018-04-19 20:46:08 +0200

    fix

--- resources/views/layouts/skel.blade.php
+++ resources/views/layouts/skel.blade.php
@@ -65,90 +65,55 @@
 </head>
 <body>
 
-    <div id="wrap">
-        <nav class="navbar navbar-expand navbar-light navbar-laravel d-none d-xl-block d-lg-block d-md-block">
-            <div class="container ">
+     <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }} </a>
-				<ul class="navbar-nav mr-auto">
-					<li><a class="nav-link" href="{{ url('/animes') }}">{{ __('Animes') }}</a></li>
-					<li><a class="nav-link" href="{{ url('/emision') }}">{{ __('Emision') }}</a></li>
-					<li><a class="nav-link" href="{{ url('/personajes') }}">{{ __('Personajes') }}</a></li>
-					<li><a class="nav-link" href="{{ url('/aleatorio') }}">{{ __('Aleatorio') }}</a></li>
-				</ul>
-				
-				<ul class="navbar-nav ml-auto">
-					<!-- Authentication Links -->
-					<form class="form-inline" action="{{ url('/busqueda') }}" method="get">
-						<input class="form-control" type="text" name="search" placeholder="{{ __('Buscar animes...')}}">
-					</form>
-					@guest
-						<li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-						<li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-					@else
+				<ul class="navbar-nav mr-auto"><li><a class="nav-link" href="{{ url('/animes') }}">{{ __('Animes') }}</a></li></ul>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-						<li class="nav-item dropdown">
-							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-								{{ Auth::user()->name }} <span class="caret"></span>
-							</a>
-						
-							<!-- Dropdown Menu-->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+						<li><a class="nav-link" href="{{ url('/emision') }}">{{ __('Emision') }}</a></li>
+						<li><a class="nav-link" href="{{ url('/personajes') }}">{{ __('Personajes') }}</a></li>
+						<li><a class="nav-link" href="{{ url('/aleatorio') }}">{{ __('Aleatorio') }}</a></li>
+					</ul>
 
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" href="{{ url('/usuario') . '/' . Auth::user()->name }} ">{{ __('Perfil') }}</a>
-								@if(Auth::user()->name === 'ShuviDola')
-								<a class="dropdown-item" href="{{ url('/administrar') }}">{{ __('Administrar') }}</a>
-								@endif
-								<a class="dropdown-item" href="{{ url('/pendientes') }}">{{ __('Pendientes') }}</a>
-								<a class="dropdown-item" href="{{ url('/terminadas') }}">{{ __('Terminadas') }}</a>
-								<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
-							</div>
-						</li>
-					@endguest
-				</ul>
-            </div>
-        </nav>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+						<form class="form-inline" action="{{ url('/busqueda') }}" method="get">
+							<input class="form-control" type="text" name="search" placeholder="{{ __('Buscar animes...')}}">
+						</form>
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                        @else
 
-        <nav class="navbar navbar-expand navbar-light navbar-laravel d-sm-block d-md-none">
-            <div class="container ">
-                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }} </a>
-				<ul class="navbar-nav mr-auto">
-					<li><a class="nav-link" href="{{ url('/animes') }}">{{ __('Animes') }}</a></li>
-					<li><a class="nav-link" href="{{ url('/emision') }}">{{ __('Emision') }}</a></li>
-					<li><a class="nav-link" href="{{ url('/personajes') }}">{{ __('Personajes') }}</a></li>
-					<li><a class="nav-link" href="{{ url('/aleatorio') }}">{{ __('Aleatorio') }}</a></li>
-				</ul>
-				
-				<ul class="navbar-nav ml-auto">
-					<!-- Authentication Links -->
-					<form class="form-inline" action="{{ url('/busqueda') }}" method="get">
-						<input class="form-control" type="text" name="search" placeholder="{{ __('Buscar animes...')}}">
-					</form>
-					@guest
-						<li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-						<li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-					@else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+							
+								<!-- Dropdown Menu-->
 
-						<li class="nav-item dropdown">
-							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-								{{ Auth::user()->name }} <span class="caret"></span>
-							</a>
-						
-							<!-- Dropdown Menu-->
-
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" href="{{ url('/usuario') . '/' . Auth::user()->name }} ">{{ __('Perfil') }}</a>
-								@if(Auth::user()->name === 'ShuviDola')
-								<a class="dropdown-item" href="{{ url('/administrar') }}">{{ __('Administrar') }}</a>
-								@endif
-								<a class="dropdown-item" href="{{ url('/pendientes') }}">{{ __('Pendientes') }}</a>
-								<a class="dropdown-item" href="{{ url('/terminadas') }}">{{ __('Terminadas') }}</a>
-								<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
-							</div>
-						</li>
-					@endguest
-				</ul>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/usuario') . '/' . Auth::user()->name }} ">{{ __('Perfil') }}</a>
+									@if(Auth::user()->name === 'ShuviDola')
+                                    <a class="dropdown-item" href="{{ url('/administrar') }}">{{ __('Administrar') }}</a>
+									@endif
+									<a class="dropdown-item" href="{{ url('/pendientes') }}">{{ __('Pendientes') }}</a>
+									<a class="dropdown-item" href="{{ url('/terminadas') }}">{{ __('Terminadas') }}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
         </nav>
 
